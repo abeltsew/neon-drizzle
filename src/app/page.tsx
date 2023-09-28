7f0eafd784eef0a85a13cf882dc518a6f33a78de
abeltsew <35835135+abeltsew@users.noreply.github.com>
2023-09-28 11:43:50 +0300

    Add shadcdn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { Button } from '@/components/ui/button';
 import { useState, useEffect } from 'react';
 
 function Profile() {
@@ -18,7 +19,8 @@ function Profile() {
   if (!data) return <p>No profile data</p>;
 
   return (
-    <div>
+    <div className="p-4">
+      <Button>Click me</Button>
       <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
   );
